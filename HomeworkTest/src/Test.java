79072bc398c4a512cb88349c0feab67a94f7d780
AdamFeffer <aafeffer@gmail.com>
2016-03-04 14:42:07 -0800

    added team select and hockey time

--- HomeworkTest/src/Test.java
+++ HomeworkTest/src/Test.java
@@ -1,9 +1,28 @@
-
+import java.util.*;
+import javax.swing.*;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("HI guys");
-
+		
+		int play = JOptionPane.showConfirmDialog(null,"HOCKEY TIME");
+		if (play == 0){
+			String teamnum1 = ("select team one");
+			String teamnum2 = ("select team two");
+			System.out.println("ya");
+			teamSelect(teamnum1);
+			teamSelect(teamnum2);
+		}
+		else{
+			JOptionPane.showConfirmDialog(null, "you have to play!!!");
+		}
 	}
-
+	public static void teamSelect(String teamnum){
+		JList list = new JList(new String[] {"LA Kings", "Anahiem ducks", "Dallas Stars"});
+		JOptionPane.showMessageDialog(
+		  null, list, teamnum, JOptionPane.PLAIN_MESSAGE);
+		System.out.println(Arrays.toString(list.getSelectedIndices()));
+}
+	public void JRadioButton(Icon icon,
+            boolean selected) {
+	}
 }
